08b4b968c5098cd9fcbc245feeaa465be083c6d2
Justin Newman <newmanjustin0@gmail.com>
2017-04-20 20:22:23 -0600

    console logs in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 alert('Welcom to Git!');
+
+console.log('We are on a new branch!');
+
+var firstName = 'Justin';
+var lastName = 'Newman';
+
+console.log('Full Name: ' + firstName + " " + lastName);
