66bae7fb88dcfd04cfc9d1d00387deb89831883e
Justin Newman <newmanjustin0@gmail.com>
2017-04-20 20:49:59 -0600

    added car input

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ if (age > 19)
     console.log('not a teen');
 else
     console.log('are a teen');
+
+
+
+var carButton = document.getElementById('get-car');
+carButton.addEventListener('click', function() {
+    var carInput = document.getElementById('car');
+    console.log(carInput.value);
+});
